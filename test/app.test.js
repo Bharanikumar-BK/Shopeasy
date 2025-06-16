c504185c79be501d66959a3bf13b102ade544002
Bharanikumar-BK <bharanikumar2911@yahoo.com>
2025-06-16 15:07:56 +0530

    Changes corrected

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest');
-const {app, server } = require('../app')
+const {app, server, name } = require('../app')
 
 describe('GET /', () => {
    it("Should return 200 status and correct message", async () => {
       const respone = await request(app).get("/");
       expect(respone.status).toBe(200);
-      expect(respone.text).toBe("Welcome to ShopEasy, From BK!");
+      expect(respone.text).toBe(`Welcome to ShopEasy, From ${name}!`);
    })
 })
 afterAll(() => {
